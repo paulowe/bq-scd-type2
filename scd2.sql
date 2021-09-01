021cdfbf9e5e9e21fb0a6b183b326f6e3fff475b
Paul Owe <paul.owe@live.com>
2021-09-01 01:53:48 -0400

    Update scd2.sql

--- scd2.sql
+++ scd2.sql
@@ -11,4 +11,24 @@ UNION ALL
 SELECT    NULL,
           `project.dataset.table_staging`.*
 FROM      `project.dataset.table_staging`
-JOIN      `project.dataset.table_staging`
+JOIN      `project.dataset.TABLE`
+ON        `project.dataset.table_staging`.product_id = `project.dataset.TABLE`.product_id
+WHERE     (
+              `project.dataset.table_staging`.quantity <> `project.dataset.TABLE`.quantity
+          AND  `project.dataset.TABLE`.end_date IS NULL)) sub
+ON sub.join_key = `project.dataset.TABLE`.product_id
+WHEN matched
+AND sub.quantity <> `project.dataset.TABLE`.quantity THEN UPDATE
+set end_date = CURRENT_DATE()
+WHEN NOT matched THEN INSERT
+       (
+              product_id,
+              product_name,
+              quantity
+       )
+        VALUES
+        (
+               sub.product_id,
+               sub.product_name,
+               sub.quantity
+         );
